51ea39cbb348bd9139204bfd09b560a1f1265bca
Your Name gchazov <cheyzov@outlook.com>
2023-06-04 21:57:53 +0500

    minor fixes

--- Lab_Work14/UnitTesting/ExtensionsAndRequestsTesting.cs
+++ Lab_Work14/UnitTesting/ExtensionsAndRequestsTesting.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UnitTesting.ExpressionsAndRequests
+namespace UnitTesting.ExtensionsAndRequestsTesting
 {
     //тестирование запросов и методов расширения
     [TestClass]
